2f5ac28f269b24fa6e12b2116ed3a5891589ba7f
Tanmoy Sarker <tanmoysarker749@outlook.com>
2019-11-13 19:48:21 +0600

    initial commit

---
+++ precache-manifest.5a7b88977e014bcad747484fa2242539.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee0a708000d28b74b350",
+    "url": "/React-portfolio/static/js/runtime~main.ee0a7080.js"
+  },
+  {
+    "revision": "66496ba37a0ec9baccb3",
+    "url": "/React-portfolio/static/js/main.66496ba3.chunk.js"
+  },
+  {
+    "revision": "faa977f242a12b5bdf51",
+    "url": "/React-portfolio/static/js/2.faa977f2.chunk.js"
+  },
+  {
+    "revision": "66496ba37a0ec9baccb3",
+    "url": "/React-portfolio/static/css/main.ce3b73e1.chunk.css"
+  },
+  {
+    "revision": "f8dead500bb40c1c98812e263256610d",
+    "url": "/React-portfolio/index.html"
+  }
+];
